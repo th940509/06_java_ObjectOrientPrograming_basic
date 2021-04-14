b676482f5c4883105020f65ad74a09d61899bd26
잉송영 <@LAPTOP-B5E5718U>
2021-04-14 18:57:53 +0900

    04/14 18:57 자습

--- 06_java_ObjectOrientPrograming_basic/src/step6_02/method/MethodEx09_내정답.java
+++ 06_java_ObjectOrientPrograming_basic/src/step6_02/method/MethodEx09_내정답.java
@@ -1,5 +1,5 @@
 package step6_02.method;
-//2021/04/14 17:25 ~ 
+//2021/04/14 17:25 ~ 18:19
 
 import java.util.Arrays;
 import java.util.Random;
@@ -22,22 +22,86 @@ class Ex09_1 {
 	}
 	
 	//문제 2)
-	void printSumAndAverage (int [] scores) {}
+	void printSumAndAverage (int [] scores) {
+		int total = 0;
+		for(int i=0; i<scores.length; i++) {
+			total += scores[i];
+		}
+		System.out.println("총점: " + total);
+		System.out.println("평균: " + (double) total / scores.length);
+		System.out.println();
+	}
 	
 	//문제 3)
-	void printWinner (int [] scores) {}
+	void printWinner (int [] scores) {
+		int count = 0;
+		for(int i=0; i<scores.length; i++) {
+			if(scores[i] >= 60) {
+				System.out.println("합격");
+				count ++;
+			}
+		}
+		System.out.println("합격생 수: " + count);
+		System.out.println();
+	}
 	
 	//문제 4)
-	void printScore1 (int [] scores) {}
+	void printScore1 (int [] scores) {
+		System.out.print("인덱스 입력: ");
+		int idx = scan.nextInt();
+		
+		for(int i=0; i<scores.length; i++) {
+			if(scores[i]==scores[idx]) {
+				System.out.println("성적: " + scores[i]);
+			}
+		}
+	}
 	
 	//문제 5)
-	void printScore2 (int [] scores) {}
+	void printScore2 (int [] scores) {
+		System.out.println("성적 입력: ");
+		int score = scan.nextInt();
+		
+		for(int i=0; i<scores.length; i++) {
+			if(scores[i] == score) {
+				System.out.println("인덱스: " + i);
+			}
+		}
+	}
 	
 	//문제 6)
-	void printScore3(int [] hakbuns, int [] scores) {}
+	void printScore3(int [] hakbuns, int [] scores) {
+		System.out.print("학번 입력: ");
+		int hakbun = scan.nextInt();
+		
+		boolean isFind = false;
+		
+		for(int i=0; i<hakbuns.length; i++) {
+			if(hakbun == hakbuns[i]) {
+				System.out.println("성적: " + scores[i]);
+				isFind = true;
+			}
+		}
+		if(isFind == false) {
+			System.out.println("학번을 다시 확인하세요!");
+		}
+		System.out.println();
+	}
 	
 	//문제 7)
-	void printNumberOne (int [] hakbuns, int [] scores) {}
+	void printNumberOne (int [] hakbuns, int [] scores) {
+		int maxNum = scores[0];
+		int maxIdx = 0;
+		
+		for(int i=0; i<scores.length; i++) {
+			if(maxNum < scores[i]) {
+				maxNum = scores[i];
+				maxIdx = i;
+			}
+		}
+		System.out.println("1등의 성적: " + maxNum);
+		System.out.println("1등의 헉번: " + hakbuns[maxIdx]);
+	}
 	
 }
 
@@ -61,20 +125,24 @@ public class MethodEx09_내정답 {
 
 		// 문제3) 성적이 60점 이상이면 합격이며 합격생 수를 출력하는 메서드
 		// 예 3) 2명
-
+		e.printWinner(scores);
+		
 		// 문제4) 인덱스를 입력받아 성적 출력하는 메서드
 		// 정답4) 인덱스 입력 : 1 성적 : 11점
-
+		e.printScore1(scores);
+		
 		// 문제5) 성적을 입력받아 인덱스 출력하는 메서드
 		// 정답5) 성적 입력 : 11 인덱스 : 1
-
+		e.printScore2(scores);
+		
 		// 문제6) 학번을 입력받아 성적 출력하는 메서드 // 단, 없는학번 입력 시 예외처리
 	    // 단, 없는학번 입력 시 예외처리
 		// 정답6) 학번 입력 : 1003 성적 : 45점
+		e.printScore3(hakbuns, scores);
 
 		// 문제7) 1등학생의 학번과 성적 출력하는 메서드
 		// 정답7) 1004번(98점)
-
+		e.printNumberOne(hakbuns, scores);
 	}
 
 }
